fd8a2ebe5c039f5395824ee1c033b8afcf31ab82
mojingzeng <mojingzeng@163.com>
2019-04-20 23:04:08 +0800

    Update excel.py

--- 测试/excel.py
+++ 测试/excel.py
@@ -8,6 +8,6 @@ wb = Workbook()
 ws = wb.get_active_sheet()
 ws.title = '99乘法表'
 for row in range(1,10):
-    for col in range(1,10):
-        ws.cell(row = row, column = col).value = row * col
-wb.save(filename = 'mul_table.xlsx')
+    for col in range(1, 10):
+        ws.cell(row=row, column=col).value = row * col
+wb.save(filename='mul_table.xlsx')
